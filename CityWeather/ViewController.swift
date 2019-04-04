8bfb79a1403f34e18a3e52f6870fe4519e79e654
Avinash Reddy <beavinash15@gmail.com>
2019-04-03 19:59:00 -0700

    Initial Commit

---
+++ CityWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CityWeather
+//
+//  Created by Avinash Reddy on 4/3/19.
+//  Copyright © 2019 Avinash Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
