33c938fce238846d1957d9a253935fab87b8d098
Avinash Reddy <beavinash15@gmail.com>
2019-04-04 00:55:16 -0700

    API Client Addition

---
+++ CityWeather/API/Weather.swift
@@ -0,0 +1,43 @@
+//
+//  Weather.swift
+//  CityWeather
+//
+//  Created by Avinash Reddy on 4/4/19.
+//  Copyright © 2019 Avinash Reddy. All rights reserved.
+//
+
+import Foundation
+
+class Weather: Codable {
+    let forecast: Forecast
+}
+
+struct Forecast: Codable {
+    let forecastText: ForecastText
+    
+    private enum CodingKeys: String, CodingKey {
+        case forecastText = "txt_forecast"
+    }
+}
+
+struct ForecastText: Codable {
+    let date: String
+    let forecastDays: [ForecastDay]
+    
+    private enum CodingKeys: String, CodingKey {
+        case date
+        case forecastDays = "forecastday"
+    }
+}
+
+struct ForecastDay: Codable {
+    let iconUrl: String
+    let day: String
+    let description: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case iconUrl = "icon_url"
+        case day = "title"
+        case description = "fcttext"
+    }
+}
